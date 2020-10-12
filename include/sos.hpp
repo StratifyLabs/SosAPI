a08bf595f168ab40f82ff068ce269f05ef493846
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-10-11 22:55:49 -0600

    initial code commit

---
+++ include/sos.hpp
@@ -0,0 +1,22 @@
+/*! \file */ // Copyright 2011-2020 Tyler Gilbert and Stratify Labs, Inc; see LICENSE.md for rights.
+
+#ifndef SOSAPI_SOS_HPP
+#define SOSAPI_SOS_HPP
+
+/*! \brief System Access (POSIX wrappers)
+ * \details The sys namespace includes classes for
+ * accessing the system.
+ *
+ */
+namespace sos {
+}
+
+#include "sos/Appfs.hpp"
+#include "sos/Auth.hpp"
+#include "sos/Sos.hpp"
+#include "sos/Sys.hpp"
+#include "sos/TaskManager.hpp"
+
+using namespace sos;
+
+#endif /* SOSAPI_SOS_HPP */
